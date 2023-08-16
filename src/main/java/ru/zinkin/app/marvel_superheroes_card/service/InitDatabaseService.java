e8c78f085147bbaec9d34baae2df82c3a35a04ff
Dmitry Zinkin <yashtlsht@mail.ru>
2023-08-17 00:47:01 +0300

    executeApp

--- src/main/java/ru/zinkin/app/marvel_superheroes_card/service/InitDatabaseService.java
+++ src/main/java/ru/zinkin/app/marvel_superheroes_card/service/InitDatabaseService.java
@@ -99,15 +99,22 @@ public class InitDatabaseService {
                 "main6.png"));
         Characters characters7 = characterDao.save(new Characters("spider-man-miles-morales",
                 "TOR Odinson",
-                76,
-                639,
+                77,
+                640,
                 List.of("Blue"),
                 List.of("Blond"),
                 "Marvel Universe",
-                List.of("God of Thunder","Goldilocks"),
-                "None",
-                "Asgard",
-                List.of("Loki","Zevs"),
+                List.of("Donald M. Blake", "God of Thunder"
+                        , "Son of Odin", "The Thunderer", "Lord of Asgard",
+                        "Jake Olson", "Sigurd Jarlson", "Donar", "Donner",
+                        "Hloriddi", "Unhappy Hrungnir’s Playmate",
+                        "Veur", "Hrodr’s Foe-Man", "Longbeard’s Son",
+                       " Vingthor the Hurler", "Siegfried", "Siegmund",
+                        "Woe-King", "(impersonated) Hercules", "Harokin",
+                        "Freya", "formerly bound to Eric Masterson"),
+                "(Thor) Tutored by scholars of Asgard, (Blake) MD",
+                "A cave in Norway",
+                List.of("Gaea (mother)", "Frigga (adoptive mother)", "Odin Borson (father, deceased)", "Sigyn (sister-in-law)", "Solveig (sister-in-law, deceased)"," Loki (adoptive brother)", "Balder", "Vidar (half-brothers)", "Vili"," Ve (paternal uncles, deceased)", "Bor Burison (paternal grandfather, deceased)", "Bestia (paternal grandmother, presumed deceased)", "Buri (Tiwaz, great- grandfather)", "Bolthorn (great-grandfather", "presumed deceased)"," Jormungand (Midgard Serpent)", "Fenris Wolf (nephew)", "Hela (niece)", "large extended family via Gaea and others"),
                 "main6.png"));
 
         comicsDao.save(new Comics(
@@ -132,7 +139,7 @@ public class InitDatabaseService {
                 List.of("Todd Nauck"),
                 new ArrayList<>()
         ));
-        Comics comics3 = comicsDao.save(new Comics(
+        comicsDao.save(new Comics(
                 "911_20th_anniversary_tribute_the_four_fives_2021_1",
                 "9/11 20th Anniversary Tribute: The Four Fives (2021) #1",
                 "In honor and in remembrance of 9/11/01.",
@@ -143,7 +150,7 @@ public class InitDatabaseService {
                 new ArrayList<>(),
                 new ArrayList<>()
         ));
-        Comics comics4= comicsDao.save(new Comics(
+        comicsDao.save(new Comics(
                 "the_vitals_true_ems_stories_2021",
                 "The Vitals: True EMS Stories (2021)",
                 "Healthcare workers are on the frontlines during this difficult time. THE VITALS tells tales of some real heroes, the EMS teams, as they travel to high-risk environments in order to rescue those in urgent need.",
@@ -154,7 +161,7 @@ public class InitDatabaseService {
                 new ArrayList<>(),
                 new ArrayList<>()
         ));
-        Comics comics5 = comicsDao.save(new Comics(
+        comicsDao.save(new Comics(
                 "109961",
                 "Spider-Man (2022) #11",
                 "(RE)INTRODUCING…SPIDER-BOY! The battle to save the Spider-Verse may be over, but spinning out of the restored Web of Life and Destiny returns the spectacular SPIDER-BOY, Peter Parker's stupendous sidekick! Wait, that can't be right - who IS this Spider-Boy, and what is his connection to the Amazing Spider-Man?!",
@@ -165,7 +172,7 @@ public class InitDatabaseService {
                 List.of("Mark Bagley"),
                 new ArrayList<>()
         ));
-        Comics comics6 = comicsDao.save(new Comics(
+        comicsDao.save(new Comics(
                 "venom_2018_1",
                 "Venom (2018) #1",
                 "In the wake of S.H.I.E.L.D.'s collapse, an ancient and primordial evil has been awakened beneath the streets of New York, and with it, something equally evil has awakened in that most Wicked of Webslingers - VENOM! Still a Lethal Protector of the innocents in New York, this never-before-seen threat could force Venom to relinquish everything it holds dear - including Eddie Brock! Join two of the hottest creators in comics today, Donny Cates and Ryan Stegman, for a VENOM adventure a thousand years in the making!",
@@ -176,7 +183,7 @@ public class InitDatabaseService {
                 new ArrayList<>(),
                 new ArrayList<>()
         ));
-        Comics comics7 = comicsDao.save(new Comics(
+        comicsDao.save(new Comics(
                 "thor_2018_1",
                 "Thor (2018) #1",
                 "JASON AARON & MIKE DEL MUNDO TAKE THE PRINCE OF ASGARD IN A WHOLE NEW DIRECTION! Thor Odinson has regained his mantle – and with it, a wild new world of trouble on his mighty hands! The artifacts of Asgard have been scattered across the earth, and to reclaim them, Thor will have to face some ugly truths. Like the production cost of hundreds of new hammers! And the Thunder God is going to need every last one of them if he’s going to stop the unstoppable Juggernaut. Jason Aaron takes the Prince of Asgard in a whole new direction with YOUNG GUN artist Mike Del Mundo joining him at the helm! And don’t miss the latest chapter of the King Thor saga with acclaimed BLACK BOLT artist Christian Ward, as the Thor of the far future encounters an old friend who’s undergone some startling changes.",
